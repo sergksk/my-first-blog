8e38a998c811cf3b1fbea7c26e6e89cefd35be72
Serg <mlpu315@mail.ru>
2018-03-02 15:36:17 +0500

    approbed coments

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -24,8 +24,11 @@ class Comment(models.Model):
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
-        self.approved_comment = True
-        self.save()
+         self.approved_comment = True
+         self.save()
+		 
+    def approved_comments(self):
+         return self.comments.filter(approved_comment=True)
 
     def __str__(self):
-        return self.text
+         return self.text
